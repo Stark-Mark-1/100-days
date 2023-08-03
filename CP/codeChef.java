703cc4f618a37dce2bec255310609c3a7bf1240d
Utkarsh-Rai22 <22utkarshrai@gmail.com>
2023-08-04 03:31:33 +0530

    Solved some CP practice questions

---
+++ CP/codeChef.java
@@ -0,0 +1,38 @@
+package CP;
+import java.util.*;
+public class codeChef{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t>0){
+            // To Do List Code Chef
+            // int n=sc.nextInt();
+            // int arr[] = new int [n];
+            // int c=0;
+            // for(int i=0;i<n;i++){
+            //     arr[i]=sc.nextInt();S
+            //     if(arr[i]>999){
+            //         c++;
+            //     }
+            // }
+            // System.out.println(c);
+            // Small Factorials Code Chef
+            // int n=sc.nextInt();
+            // int val =1;
+            // for(int i =1;i<=n;i++){
+            //     val *=i;
+            // }
+            // System.out.println(val);
+            //lucky four
+            int n=sc.nextInt();
+            int b=0;
+            String a = Integer.toString(n);
+            for(int i=0; i<a.length();i++){
+                if(a.charAt(i)=='4'){
+                    b++;
+                }
+            }System.out.println(b);
+            t--;
+        }
+    }
+}
